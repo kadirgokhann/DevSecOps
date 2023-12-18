e19878c4bfb0ba6ba85b18c5e8b3649f9d18bcca
Kadir Gökhan Sezer <kadirgokhansezer@Kadirs-MacBook-Air-3.local>
2023-12-18 14:15:25 +0300

    asda

--- main/Mainfunction.java
+++ main/Mainfunction.java
@@ -20,7 +20,7 @@ import java.io.IOException;
  */
 public final class Mainfunction {
     /**
-     * Number of lines in the outputsad file.ssfsfsdsdssdsadsdssdsd
+     * Number of lines in the outputsad file.ssfsfsdsdssdsadsasdasddssdsd
      */
     public static final int LINES = 6;
     /**
