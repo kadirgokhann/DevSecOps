f1acaf56a96909a578b11558c5146b72879b8262
Kadir Gökhan Sezer <kadirgokhansezer@192.168.1.60>
2023-12-23 12:00:32 +0300

    asd

--- main/Mainfunction.java
+++ main/Mainfunction.java
@@ -20,7 +20,7 @@ import java.io.IOException;
  */
 public final class Mainfunction {
     /**
-     * Number of lines in the outputsad file.sssdsasffsffsaf.
+     * Number of lines in the outputsad file.sssdsasdsasffsffsaf.
      */
     public static final int LINES = 6;
     /**
